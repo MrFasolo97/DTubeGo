1bef00b1184395e49c6b9e70cd6bef41178b94bd
MrFasolo97 <79fasolo97@gmail.com>
2023-07-12 21:46:16 +0200

    Made changes to API node discovery, now able to handle offline and slow servers

--- lib/utils/Avalon/discoverAPINode.dart
+++ lib/utils/Avalon/discoverAPINode.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dtube_go/res/Config/APINodesConfigValues.dart';
@@ -18,7 +19,7 @@ Future<String> discoverAPINode() async {
   Map<String, int> _sortedApiNodesByResponseTime = {};
   Map<String, int> _ApiNodesOnError = {};
   // as long as we do not have received any response within the configured timeout
-  // TODO: do this only for X times and then responde to the user about missing internet or blockchain issues
+  // TODO: do this only for X times and then respond to the user about missing internet or blockchain issues
   do {
     _retries =
         _retries + 1; // every retry of the node list will increase the timeout
@@ -44,15 +45,26 @@ Future<String> discoverAPINode() async {
           log(node + ": " + response.statusCode.toString());
           _ApiNodesOnError[node] = -1;
         } else if (response.statusCode == 200) {
+          var _afterRequestMicroSeconds = DateTime
+              .now()
+              .microsecondsSinceEpoch;
+          log(_afterRequestMicroSeconds.toString());
           // node responded
           // save responsetime to list
-          var _afterRequestMicroSeconds = DateTime.now().microsecondsSinceEpoch;
-          log(_afterRequestMicroSeconds.toString());
-
-          _nodeResponses[node] =
-              _afterRequestMicroSeconds - _beforeRequestMicroSeconds;
+          int? count = await json.decode(await response.body)['count'] as int;
+          await count;
+          if(count<=0) {
+            _ApiNodesOnError[node] = -1;
+          } else {
+            _nodeResponses[node] =
+                _afterRequestMicroSeconds - _beforeRequestMicroSeconds;
+          }
+        } else if(response.statusCode == 408 && _nodeResponses.length > 0) {
+          _ApiNodesOnError[node] = -1;
         }
       } catch (e) {
+          print(e);
+          _ApiNodesOnError[node] = -1;
       }
       if (_retries > 20) {
         break;
@@ -61,10 +73,11 @@ Future<String> discoverAPINode() async {
   } while (_nodeResponses.length ==
       0 && _nodes.length > _ApiNodesOnError.length); // as long as no node responded in specified timeout
 // sort all responses by their response time
+  if(_ApiNodesOnError.isNotEmpty) {
+    log("Nodes on error: " + _ApiNodesOnError.keys.toString());
+  }
   _sortedApiNodesByResponseTime = SplayTreeMap.from(_nodeResponses,
       (key1, key2) => _nodeResponses[key1]!.compareTo(_nodeResponses[key2]!));
-
   log("using " + _sortedApiNodesByResponseTime.entries.toList()[0].key);
-  // pick quickest node
   return _sortedApiNodesByResponseTime.entries.toList()[0].key;
 }
