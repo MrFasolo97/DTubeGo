630df722cae973edc9996033387e294ec7ded864
MrFasolo97 <79fasolo97@gmail.com>
2023-12-19 21:36:34 +0100

    Implemented unsupported and too old versions check code.

--- lib/ui/startup/Startup.dart
+++ lib/ui/startup/Startup.dart
@@ -4,10 +4,13 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:ovh.fso.dtubego/ui/widgets/DialogTemplates/DialogWithTitleLogo.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:ovh.fso.dtubego/utils/GlobalStorage/globalVariables.dart' as globals;
+import 'package:ovh.fso.dtubego/utils/GlobalStorage/globalVariables.dart'
+    as globals;
 
 import 'package:ovh.fso.dtubego/bloc/settings/settings_bloc_full.dart';
 import 'package:ovh.fso.dtubego/ui/startup/PinPad.dart';
@@ -44,22 +47,155 @@ class _StartUpState extends State<StartUp> {
   StatefulWidget build(BuildContext context) {
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
+        var versions;
         () async {
-          var versions;
           try {
             versions = await jsonDecode((await Dio().get(
-                "https://raw.githubusercontent.com/MrFasolo97/DTubeGo/master/versions.json"))
-                .data) ?? {};
-        } catch(e) {
+                        "https://raw.githubusercontent.com/MrFasolo97/DTubeGo/master/versions.json"))
+                    .data) ??
+                {};
+          } catch (e) {
             log(e.toString());
             versions = {};
-        }
-          final packageInfo = await PackageInfo.fromPlatform();
-          if(versions[packageInfo.version] == null || versions[packageInfo.version].disabled == null || versions[packageInfo.version].disabled == false) {
-            log("Correctly using version "+packageInfo.version);
+          }
+          var packageInfo = await PackageInfo.fromPlatform();
+          if (await versions[packageInfo.version] == null ||
+              versions[packageInfo.version]["disabled"] == null ||
+              versions[packageInfo.version]["disabled"] == false) {
           } else {
-            log("Exiting as this version is too old");
-            exit(0);
+            showDialog(
+                builder: (context) => PopUpDialogWithTitleLogo(
+                    titleWidget: Text("Too old"),
+                    callbackOK: () async {
+                      await SystemNavigator.pop();
+                    },
+                    callbackCancel: () async {
+                      await SystemNavigator.pop();
+                    },
+                    titleWidgetPadding: 10.h,
+                    titleWidgetSize: 20.w,
+                    showTitleWidget: false,
+                    child: SingleChildScrollView(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                                "Exiting as this version is too old, please, update.",
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                                textAlign: TextAlign.center),
+                          ),
+                          SizedBox(height: 2.h),
+                          SizedBox(height: 2.h),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                    child: InkWell(
+                                        child: Container(
+                                          padding: EdgeInsets.only(
+                                              left: 4.w,
+                                              top: 20.0,
+                                              bottom: 20.0,
+                                              right: 4.w),
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(
+                                              color: globalRed,
+                                              borderRadius: BorderRadius.only(
+                                                bottomLeft:
+                                                    Radius.circular(20.0),
+                                                bottomRight:
+                                                    Radius.circular(20.0),
+                                              )),
+                                          child: Text(
+                                            "Ok",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headlineMedium,
+                                            textAlign: TextAlign.right,
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          log("Exiting as this version is too old.");
+                                          SystemNavigator.pop();
+                                        })),
+                              ])
+                        ]))),
+                context: context);
+          }
+        }();
+        () async {
+          var packageInfo = await PackageInfo.fromPlatform();
+          if (versions[packageInfo.version] == null ||
+              versions[packageInfo.version]["unsupported"] == null ||
+              versions[packageInfo.version]["unsupported"] == false) {
+            log("Correctly using version " + packageInfo.version);
+          } else {
+            showDialog(
+                builder: (context) => PopUpDialogWithTitleLogo(
+                    titleWidget: Text("Unsupported"),
+                    callbackOK: () async {
+                      await SystemNavigator.pop();
+                    },
+                    callbackCancel: () async {
+                      await SystemNavigator.pop();
+                    },
+                    titleWidgetPadding: 10.h,
+                    titleWidgetSize: 20.w,
+                    showTitleWidget: false,
+                    child: SingleChildScrollView(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                                "This version is unsupported, please, update as soon as possible.",
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                                textAlign: TextAlign.center),
+                          ),
+                          SizedBox(height: 2.h),
+                          SizedBox(height: 2.h),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                    child: InkWell(
+                                        child: Container(
+                                          padding: EdgeInsets.only(
+                                              left: 4.w,
+                                              top: 20.0,
+                                              bottom: 20.0,
+                                              right: 4.w),
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(
+                                              color: globalRed,
+                                              borderRadius: BorderRadius.only(
+                                                bottomLeft:
+                                                    Radius.circular(20.0),
+                                                bottomRight:
+                                                    Radius.circular(20.0),
+                                              )),
+                                          child: Text(
+                                            "Ok",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headlineMedium,
+                                            textAlign: TextAlign.right,
+                                          ),
+                                        ),
+                                        onTap: () {
+                                        })),
+                              ])
+                        ]))),
+                context: context);
           }
         }();
         // if the user has been authenticated before using login credentials
