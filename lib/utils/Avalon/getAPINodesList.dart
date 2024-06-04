9056319b2f53e2c4825aabd462f90058720b09ac
MrFasolo97 <79fasolo97@gmail.com>
2024-06-05 00:14:27 +0200

    Removed unnecessary imports and some out-of-place comments.

--- lib/utils/Avalon/getAPINodesList.dart
+++ lib/utils/Avalon/getAPINodesList.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:ovh.fso.dtubego/res/Config/APINodesConfigValues.dart';
 import 'package:http/http.dart' as http;
-
-import 'dart:collection';
 
 Future<List> getAPINodesList() async {
   List? apiNodes;
@@ -12,7 +9,7 @@ Future<List> getAPINodesList() async {
       .get(Uri.parse(
           'https://raw.githubusercontent.com/dtubego/dtube/main/public/DTube_files/avalon_apis.json'))
       .timeout(Duration(milliseconds: 5000), onTimeout: () {
-// timeout occurred
+    // timeout occurred
     return http.Response('Error', 408);
   });
   if (response.statusCode >= 400 && response.statusCode != 408) {
@@ -20,8 +17,6 @@ Future<List> getAPINodesList() async {
   } else if (response.statusCode == 200) {
     var _afterRequestMicroSeconds = DateTime.now().microsecondsSinceEpoch;
     log(_afterRequestMicroSeconds.toString());
-// node responded
-// save responsetime to list
     apiNodes = await json.decode(await response.body) as List;
     await apiNodes;
   }
